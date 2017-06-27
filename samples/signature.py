47273e5e1b7a513e8206f8b8b09a049ecace0dd2
Brett <brett@hippodigital.co.uk>
2017-06-27 10:28:43 +0100

    Updated signature.py to retrieve private key ID from card and sign

--- samples/signature.py
+++ samples/signature.py
@@ -25,26 +25,59 @@ pkcs11 = PyKCS11Lib()
 pkcs11.load()  # define environment variable PYKCS11LIB=YourPKCS11Lib
 
 # get 3rd slot
-slot = pkcs11.getSlotList()[0]
+slots = pkcs11.getSlotList()
+
+for s in slots:
+    print(s)
+
+slot = slots[0]
+
+
+#quit()
 
 session = pkcs11.openSession(slot, CKF_SERIAL_SESSION | CKF_RW_SESSION)
-session.login("1234")
+session.login("1234", CKU_USER)
+
+
+keyId = ()
+
+
+private_keys = session.findObjects([(CKA_CLASS, CKO_PRIVATE_KEY),])
+
+for private_key in private_keys:
+  key_info = private_key.to_dict()
+  if len(key_info['CKA_LABEL']) > 21:
+    print(key_info['CKA_LABEL'])
+    print(key_info['CKA_ID'])
+    print(len(key_info['CKA_LABEL']))
+    print("")
+
+    keyID = key_info['CKA_ID']
+
+
+
+
+
+
+
 
 # key ID in hex (has to be tuple, that's why trailing comma)
-keyID = (0x22,)
+#keyID = (,)
 
 # "Hello world" in hex
 toSign = "48656c6c6f20776f726c640d0a"
 
 # find private key and compute signature
+print(session.findObjects([(CKA_CLASS, CKO_PRIVATE_KEY), (CKA_ID, keyID)]))
+
 privKey = session.findObjects([(CKA_CLASS, CKO_PRIVATE_KEY), (CKA_ID, keyID)])[0]
 signature = session.sign(privKey, binascii.unhexlify(toSign), Mechanism(CKM_SHA1_RSA_PKCS, None))
-print("\nsignature: " + binascii.hexlify(bytearray(signature)))
+print("\nsignature: %s" % binascii.hexlify(bytearray(signature)))
 
 # find public key and verify signature
 pubKey = session.findObjects([(CKA_CLASS, CKO_PUBLIC_KEY), (CKA_ID, keyID)])[0]
 result = session.verify(pubKey, binascii.unhexlify(toSign), signature, Mechanism(CKM_SHA1_RSA_PKCS, None))
-print("\nVerified:", result)
+print("\nVerified: %s" % result)
 
 # logout
 session.logout()
